958a8bf61cc4580e27ec026e8cf36ab8c4e9bbb6
[dyad] <mark@jumex.org>
2025-11-02 14:52:37 -0700

    [dyad] Removed missing globals.css import from main.tsx - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./globals.css";
+// import "./globals.css"; // Removed as per static site conversion
 
 createRoot(document.getElementById("root")!).render(<App />);
