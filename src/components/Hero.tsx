55b832c39e105b4a565feda2e120f90971095001
[dyad] <mark@jumex.org>
2025-11-02 15:26:03 -0700

    [dyad] Updated Hero component with new background image - wrote 2 file(s)

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,9 +7,8 @@ const Hero = () => {
   return (
     <div
       className="relative h-[60vh] md:h-[70vh] flex items-center justify-center text-white overflow-hidden bg-cover bg-center"
-      style={{ backgroundImage: 'url("/images/ComfyUI_00004_.png")' }} // Updated image path
+      style={{ backgroundImage: 'url("/images/ComfyUI_00006_.png")' }} // Updated image path
     >
-      {/* Removed the absolute inset-0 bg-black opacity-50 div */}
       <div className="container mx-auto px-4 relative z-10 animate-fade-in">
         <div className="max-w-4xl text-center md:text-left">
           {/* Content removed as per previous requests */}
